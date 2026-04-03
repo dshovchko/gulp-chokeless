5e630a232d14e90922e026bf4553e0511b0bb1d0
Dmytro Shovchko <d.shovchko@gmail.com>
2026-04-03 13:52:12 +0300

    docs(example): use package name in imports instead of local dist path

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,22 +1,32 @@
 
-...
+import gulp from 'gulp';
+import path from 'path';
+import createPool from 'gulp-chokeless';
 
-const less = createChokelessTask({
-  workerPath: path.resolve(import.meta.dirname, './worker/less.js')
+// 1. Setup the gulp-chokeless worker pool
+const lessTask = createPool({
+  workerPath: path.resolve(import.meta.dirname, './worker/less.js'),
+  concurrency: 4
 });
 
-gulp.src(src, {
+// 2. Define your typical gulp task
+export function buildStyles() {
+  const src = 'src/styles/**/*.less';
+  const destFolder = 'dist/styles';
+  const enableSourcemap = true;
+
+  return gulp.src(src, {
     allowEmpty: true,
     sourcemaps: enableSourcemap
   })
-  .pipe(less({
+  .pipe(lessTask({
     workerOptions: {
-      less: lessOptions,
-      lightningcss: lightningcssOptions,
-      banner: { text: _banner }
+      less: { math: 'always' },
+      lightningcss: { minify: true },
+      banner: { text: "/* My Gulp Plugin Banner */" }
     }
   }))
-  .pipe(gulp.dest(destFolder, {sourcemaps: enableSourcemap ? '.' : false}));
-};
+  .pipe(gulp.dest(destFolder, { sourcemaps: enableSourcemap ? '.' : false }));
+}
 
-...
+export default buildStyles;
